96c530708b25b4d1b16bcd32bd4a35927423ca87
ershov-ilya <ershov.ilya@gmail.com>
2015-03-02 10:57:56 +0300

    Класс RESTful

---
+++ restful.class.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ershov-ilya
+ * Website: ershov.pw
+ * GitHub : https://github.com/ershov-ilya
+ * Date: 25.01.2015
+ * Time: 12:52
+ */
+class RESTful {
+    private $private_scope;
+    public $scope;
+    static $filter;
+
+    function __construct($ACTION='', $filter=array('ACTION','RESTful','id'), $arrSanitize=array()){
+        defined('ACTION') or define('ACTION', $ACTION);
+
+        RESTful::$filter = $filter;
+        // Define RESTful type
+        if(isset($_SERVER['argc'])) define('RESTful', 'CONSOLE');//$this->private_name='CONSOLE';
+        elseif(isset($_SERVER['REQUEST_RESTful']))  define('RESTful', $_SERVER['REQUEST_RESTful']); //$this->private_name=$_SERVER['REQUEST_RESTful'];
+        else{
+            define('RESTful', 'UNKNOWN');
+        }
+
+        // Combine parameters
+        $this->private_scope = array();
+        if(RESTful=='CONSOLE') {
+            //$this->private_scope = array_merge($this->private_scope, $_SERVER['argv']);
+            $this->private_scope = array_merge($this->private_scope, getOptions());
+        }
+        else{
+            $this->private_scope = array_merge($this->private_scope, $_REQUEST);
+            $this->private_scope = array_merge($this->private_scope, $this->parseRequestHeaders());
+        }
+
+
+        $this->private_scope['ACTION']=$ACTION;
+        $this->private_scope['RESTful']=RESTful;
+        // Для дебага, возможность переопределять метод
+        if(DEBUG && isset($_GET['RESTful'])) $this->private_scope['RESTful']=$_GET['RESTful'];
+
+        $this->scope = $this->sanitize($this->filtrateScope(), $arrSanitize);
+        return $this->private_scope;
+    }
+
+    function getRaw(){
+        return $this->private_scope;
+    }
+
+    function parseRequestHeaders() {
+        $headers = array();
+        foreach($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) <> 'HTTP_') {
+                continue;
+            }
+            $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+            $headers[$header] = $value;
+        }
+        return $headers;
+    }
+
+    function sanitize($arr, $filter=array()){
+        $out=array();
+        foreach($arr as $key => $val) {
+            if(isset($filter[$key])){
+                $out[$key] = filter_var($val, $filter[$key]);
+                continue;
+            }
+            switch ($key) {
+                case 'sc':
+                case 'user':
+                case 'login':
+                    // Only ALLOWED SYMBOLS
+                    $out[$key] = preg_replace('/[^a-zA-Z0-9\-_\.]+/i', '', $val);
+                    break;
+                case 'RESTful':
+                case 'ACTION':
+                    $out[$key] = preg_replace('/[^a-zA-Z\-_\.]+/i', '', $val);
+                    break;
+//                case 'phone':
+//                    $out[$key] = preg_replace('/[^0-9\s\(\)\-_\.,\+]+/i', '', $val);
+//                    break;
+                case 'pass':
+                case 'name':
+                case 'surname':
+                    $out[$key] = filter_var($val, FILTER_SANITIZE_STRING);
+                    break;
+                case 'phone':
+                case 'id':
+                    $out[$key] = filter_var($val, FILTER_SANITIZE_NUMBER_INT);
+                    break;
+                case 'email':
+                    $out[$key] = filter_var($val, FILTER_SANITIZE_EMAIL);
+                    break;
+                default:
+                    //$out[$key] = $val;
+                    $out[$key] = filter_var($val, FILTER_SANITIZE_STRING);
+            }
+        }
+        return $out;
+    }
+
+    function filtrate($arr, $filter=NULL){
+        if($filter==NULL) $filter=RESTful::$filter;
+//        if(DEBUG) {
+//            if (is_array($filter)) print "Filter type Array\n";
+//            if (is_string($filter)) print "Filter type String\n";
+//        }
+        if (is_string($filter)){
+            $filter=explode(',',$filter);
+        }
+//        if(DEBUG) print_r($filter);
+
+        $res=array();
+        foreach($filter as $el){
+            $el_cropspace=preg_replace('/ /','',$el);
+            if(isset($arr[$el_cropspace])) $res[$el_cropspace]=$arr[$el_cropspace]; // Вырезаем пробелы из имён параметров
+            if(isset($arr[$el])) $res[$el]=$arr[$el];
+        }
+        return $res;
+    }
+
+    function filtrateScope($filter=NULL){
+        return $this->filtrate($this->private_scope,$filter);
+    }
+
+    function randomString($length = 12, $charSet='') {
+        if(!empty($charSet )) $characters = $charSet;
+        else $characters = '0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+} // class RESTful
+
+function getOptions($shortopts="", $longopts  = array("id:","action:")){
+    // Скрипт example.php
+//    $shortopts  = "";
+//    $shortopts .= "a:";  // Обязательное значение
+//    $shortopts .= "v::"; // Необязательное значение
+//    $shortopts .= "abc"; // Эти параметры не принимают никаких значений
+
+//    $longopts  = array(
+//        "id:",     // Обязательное значение
+//        "action:"
+//        "action::",    // Необязательное значение
+//        "option",        // Нет значения
+//        "opt",           // Нет значения
+//    );
+    $options = getopt($shortopts, $longopts);
+    return $options;
+}
